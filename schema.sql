b267e7156113244b53fb1125659b322dd5f639f8
Maksym Dudka <dudkamv@gmail.com>
2022-08-13 16:23:30 +0300

    Add user and fix enum

--- schema.sql
+++ schema.sql
@@ -3,9 +3,20 @@
 
 create extension if not exists "uuid-ossp" with schema public;
 
-create type currency as enum ('UAN', 'USD');
+create type currency as enum ('UAH', 'USD');
 create type folder_type as enum ('CARD', 'CASH');
 create type income_category as enum ('FOOD', 'CAFE');
+create type gender as enum ('UNDEFINED', 'MALE', 'FEMALE');
+
+create table customer (
+    id uuid not null,
+    email text unique not null,
+    birthday time,
+    nick_name text,
+    age smallint,
+    gender gender default 'UNDEFINED',
+    created_at timestamptz
+);
 
 create table folder (
     id uuid default public.uuid_generate_v4() primary key,
@@ -13,28 +24,30 @@ create table folder (
     title text,
     folder_type folder_type,
     currency currency,
-    created_at timestamp
+    created_at timestamptz
 );
 
 create table customer_folder (
-    customer_id uuid not null,
-    folder_id uuid not null references folder(id),
+    customer_id uuid not null references customer,
+    folder_id uuid not null references folder,
     constraint customer_budget_id primary key (customer_id, folder_id)
 );
 
 create table invite (
     folder_owner_id uuid not null,
-    folder_id uuid not null references folder(id),
+    folder_id uuid not null references folder,
     invited_customer_id uuid not null,
+    created_at timestamptz,
     constraint invite_id primary key (folder_owner_id, folder_id, invited_customer_id)
 );
 
 create table income (
     id uuid default public.uuid_generate_v4() primary key,
     title text,
-    folder_id uuid references folder(id) on delete cascade,
-    category_id income_category,
-    customer_id uuid not null,
-    create_at timestamp,
-    amount decimal not null
+    folder_id uuid references folder on delete cascade,
+    income_category income_category,
+    customer_id uuid not null references customer,
+    created_at timestamptz,
+    amount decimal not null,
+    timezone smallint
 )
