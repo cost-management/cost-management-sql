9153535bec807e5df6ff9f9af5d9f7955c526e9b
Maksym Dudka <dudkamv@gmail.com>
2022-08-22 00:32:14 +0300

    Add new enums, add new constraints, rewrite customer and user relations

--- schema.sql
+++ schema.sql
@@ -7,7 +7,8 @@ create type currency as enum ('UAH', 'USD');
 create type folder_type as enum ('CARD', 'CASH');
 create type income_category as enum ('FOOD', 'CAFE');
 create type gender as enum ('UNDEFINED', 'MALE', 'FEMALE');
-create type invited_customer_role as enum ('ADMIN', 'USER');
+create type customer_folder_role as enum ('OWNER', 'ADMIN', 'USER');
+create type folder_skin as enum ('BLUE', 'GREEN', 'RED');
 
 create table customer (
     id uuid primary key not null,
@@ -15,22 +16,25 @@ create table customer (
     birthday time,
     nick_name text,
     age smallint,
-    gender gender default 'UNDEFINED',
-    created_at timestamptz
+    gender gender not null default 'UNDEFINED',
+    created_at timestamptz default now()
 );
 
 create table folder (
     id uuid default public.uuid_generate_v4() primary key,
-    owner_id uuid not null,
     title text,
-    folder_type folder_type,
-    currency currency,
-    created_at timestamptz
+    folder_type folder_type not null default 'CARD',
+    units bigint not null default 0,
+    nanos smallint not null default 0,
+    currency currency not null default 'UAH',
+    skin folder_skin not null default 'BLUE',
+    created_at timestamptz default now()
 );
 
 create table customer_folder (
-    customer_id uuid not null references customer,
-    folder_id uuid not null references folder,
+    customer_id uuid not null references customer on delete cascade,
+    folder_id uuid not null references folder on delete cascade,
+    customer_role customer_folder_role not null default 'OWNER',
     constraint customer_budget_id primary key (customer_id, folder_id)
 );
 
@@ -38,7 +42,7 @@ create table invite (
     folder_owner_id uuid not null,
     folder_id uuid not null references folder on delete cascade,
     invited_customer_id uuid not null,
-    created_at timestamptz,
+    created_at timestamptz default now(),
     constraint invite_id primary key (folder_owner_id, folder_id, invited_customer_id)
 );
 
@@ -46,9 +50,30 @@ create table income (
     id uuid default public.uuid_generate_v4() primary key,
     title text,
     folder_id uuid references folder on delete cascade,
-    income_category income_category,
+    income_category income_category not null,
     customer_id uuid not null references customer,
-    created_at timestamptz,
-    amount double precision not null,
-    timezone smallint
-)
+    created_at timestamptz default now(),
+    units bigint not null default 0,
+    nanos smallint not null default 0,
+    timezone smallint not null default 0
+);
+
+CREATE FUNCTION update_amount() RETURNS trigger AS $trigger_bound$
+BEGIN
+    UPDATE folder SET units = NEW.units + folder.units;
+    update folder set nanos = NEW.nanos + folder.nanos;
+
+    RETURN NEW;
+END;
+$trigger_bound$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER folder_amount_update
+
+  AFTER INSERT
+
+  ON income
+
+  FOR EACH ROW
+
+  execute procedure update_amount();
